7570942c94a21d04a346e9889ba99be881f7351a
Konstanten Komkov <volt1003@yandex.ru>
2021-04-16 16:04:46 +0300

    Rename ws_id on number. Rename city_id on ws_id - identifier of weather station in database. Add data type: 0 - weather station, 1 - METAR, 2 - weather sensor.

--- classes.py
+++ classes.py
@@ -3,28 +3,30 @@ from datetime import date
 
 
 class WeatherStation(BaseModel):
-    link: str
-    ws_id: int = None
-    country: str = None
-    city_id: int = None
     city: str = None
+    link: str
+    # 0 - weather station, 1 - metar, 2 - weather sensor
+    data_type: int
+    number: int = None
+    country: str = None
+    ws_id: int = None
     latitude: float = None
     longitude: float = None
     start_date: date = None
 
     # '%Y, %-m, %d' if you are using Unix system
     def __str__(self):
-        return f"WeatherStation(ws_id={self.ws_id}, country=\"{self.country}\", city=\"{self.city}\", " \
-               f"city_id={self.city_id}, latitude={self.latitude}, longitude={self.longitude}, " \
-               f"start_date=date({self.start_date.strftime('%Y, %#m, %d')}))"
+        return f"WeatherStation(city=\"{self.city}\", link=\"{self.link}\", data_type={self.data_type}," \
+               f"number={self.number}, country=\"{self.country}\", ws_id={self.ws_id}, latitude={self.latitude}, " \
+               f"longitude={self.longitude}, start_date=date({self.start_date.strftime('%Y, %#m, %d')}))"
 
     def __repr__(self):
-        return f"WeatherStation(ws_id={self.ws_id}, country=\"{self.country}\", city=\"{self.city}\", " \
-               f"city_id={self.city_id}, latitude={self.latitude}, longitude={self.longitude}, " \
-               f"start_date=date({self.start_date.strftime('%Y, %#m, %d')}))"
+        return f"WeatherStation(city=\"{self.city}\", link=\"{self.link}\", data_type={self.data_type}," \
+               f"number={self.number}, country=\"{self.country}\", ws_id={self.ws_id}, latitude={self.latitude}, " \
+               f"longitude={self.longitude}, start_date=date({self.start_date.strftime('%Y, %#m, %d')}))"
 
     def to_csv(self, delimiter):
-        return f"{self.city}{delimiter}{self.link}{delimiter}" \
+        return f"{self.city}{delimiter}{self.link}{delimiter}{self.data_type}{delimiter}" \
                f"{'None' if self.start_date is None else self.start_date.strftime('%Y-%m-%d')}" \
-               f"{delimiter}{self.ws_id}{delimiter}{self.country}{delimiter}{self.city_id}{delimiter}" \
+               f"{delimiter}{self.number}{delimiter}{self.country}{delimiter}{self.ws_id}{delimiter}" \
                f"{self.latitude}{delimiter}{self.longitude}"
